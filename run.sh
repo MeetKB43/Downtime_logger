1b73782b4f5cc1cba2693ff685c3e1e9adb87d5a
Meet <meetkevadiya037@gmail.com>
2026-04-20 17:57:42 -0400

    Added app.py

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /home/user/Documents/Downtime_logger
+cd /home/admin/Documents/Downtime_logger
 source venv/bin/activate
+
+LOCKFILE="/tmp/dt_logger.lock"
+exec 200>$LOCKFILE
+
+flock -n 200 || {
+	echo "Script already running. Exiting..."
+	sleep 5
+	exit 1
+}
+
 python src/test_index.py
--- src/main.py
+++ src/main.py
@@ -2,27 +2,27 @@ from gpiozero import Button
 from datetime import datetime
 import time
 import os
-#from Python_Excel_API import save_to_excel
+from dotenv import load_dotenv
 from supabase_API import record_DT
 
+Filler_Running_PIN: int = os.getenv("GPIO_Filler_Running_PIN")
 timer_running = False
 start_time = None
+Filler_running = Button(Filler_Running_PIN,pull_down = True)
 
 print("Running... Waiting for DT")
 
 
 while True:
-	s = input("Enter input")
-	
 	#Timer Start Condition
-	if s=="s" and not timer_running:
+	if not Filler_running.is_pressed and not timer_running:
 		start_time = datetime.now().replace(microsecond=0)
 		timer_running = True
 		print("Downtime Started")
 		time.sleep(0.5)
 		
 	#Timer Stop Condition
-	if s=="e" and timer_running:
+	if Filler_running.is_pressed and timer_running:
 		end_time = datetime.now().replace(microsecond=0)
 		duration = int(((end_time - start_time).total_seconds())/60)
 		print("Downtime recorded")
@@ -34,8 +34,3 @@ while True:
 		start_time = None
 		
 		time.sleep(0.5)
-	
-	#Close loop
-	if s=="t":
-		break
-	time.sleep(0.1)
